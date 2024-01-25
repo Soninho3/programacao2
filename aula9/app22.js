0138ad5077e863a9ff602cd103adb3ba5087051c
Soninho3 <miguelmachado.en2@gmail.com>
2024-01-25 21:56:19 +0000

    aula 9

---
+++ aula9/app22.js
@@ -0,0 +1,92 @@
+
+
+window.onload = () => {
+    // console.log("hello world");
+    // let arrb = add(2, 3);
+    
+    // console.log(arrb);
+    
+    // let agr = Multiply(2, 6);
+    // let nettt = Multiply(2, 6) * 2;
+    
+    // console.log(agr);
+    // console.log(nettt);
+    // recursive(3);
+
+    // console.log(twotimes());
+    // const arraj = [2,45,34,2,54,2000,23.5];
+    // console.log(fuc(arraj));
+
+    // const abiba = [
+    //     2,3,4,5,6,7,8
+    // ];
+    
+    // for(let a = 0; a <= abiba.length - 1 ; a++ ){
+    //     console.log(abiba[a]);
+    // }
+    const carlos = new test;
+    const value = carlos.imreturning2()
+    console.log(value); 
+
+}
+
+
+
+// const add = (a, b) => {
+//     return a + b; 
+// }
+
+// const Multiply = (c, b) => {
+//     return c * b; 
+// }
+
+
+// const recursive = (a) => {
+//     console.log("hello");
+//     if(a === 0){
+//         return
+//     }
+//     recursive(a-1);
+// }
+
+// const twotimes = () => {
+//     return Multiply(2, 6) * 2;
+// }
+
+
+// const fuc = (a) => {
+//     // let r = a.length;
+//     let b = 0
+//     a.forEach(element => {
+//         b += element;
+        
+//     });
+//     return b / a.length;
+
+// }
+
+// const fuc = (a) => {
+//     let ak = 0;
+//     for(let g = 0; g <= a.length; g++ ){
+
+//         ak += a[g];
+
+//     } 
+//     return ak / a.length;
+// } 
+// const abiba = [
+//     2,3,4,5,6,7,8
+// ];
+
+// for(let a = 0; a < abiba.length; a++ ){
+//     console.log(abiba[a]);
+// }
+
+class test {
+    constructor(){
+
+    }
+    imreturning2(){
+        return 2; 
+    }
+}
